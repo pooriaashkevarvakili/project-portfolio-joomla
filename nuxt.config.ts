de10b441e49c884b49ab67434cce8eaaa28a6e45
pooriavakili09 <pooriavakili09@gmail.com>
2022-11-29 21:12:07 +0330

    ssg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     css: ['@/assets/iransans.css'],
+    nitro: {
+        prerender: {
+            crawlLinks: true
+        }
+    },
     modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt']
 })
